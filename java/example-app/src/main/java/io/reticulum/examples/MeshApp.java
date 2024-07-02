7f67f0b20d3b4cdedc5119f2a309ba9b1d81712f
Jürg Schulthess <schulthess@puzzle.ch>
2024-07-02 21:23:32 +0200

    initial implementation of mesh

---
+++ java/example-app/src/main/java/io/reticulum/examples/MeshApp.java
@@ -0,0 +1,354 @@
+package io.reticulum.examples;
+
+import io.reticulum.Reticulum;
+import io.reticulum.Transport;
+import io.reticulum.destination.Destination;
+import io.reticulum.destination.DestinationType;
+import io.reticulum.destination.Direction;
+import io.reticulum.destination.ProofStrategy;
+//import io.reticulum.destination.ProofStrategy;
+import io.reticulum.identity.Identity;
+import io.reticulum.link.Link;
+//import io.reticulum.constant.LinkConstant;
+import io.reticulum.packet.Packet;
+import io.reticulum.transport.AnnounceHandler;
+import static io.reticulum.link.TeardownSession.DESTINATION_CLOSED;
+import static io.reticulum.link.TeardownSession.INITIATOR_CLOSED;
+import static io.reticulum.link.TeardownSession.TIMEOUT;
+import static io.reticulum.link.LinkStatus.ACTIVE;
+import static io.reticulum.identity.IdentityKnownDestination.recall;
+//import static io.reticulum.constant.ReticulumConstant.TRUNCATED_HASHLENGTH;
+import lombok.extern.slf4j.Slf4j;
+import lombok.Data;
+//import lombok.Setter;
+//import lombok.Getter;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+//import static java.util.Objects.isNull;
+import static java.util.Objects.nonNull;
+//import static org.apache.commons.lang3.BooleanUtils.isFalse;
+
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+//import java.util.concurrent.TimeUnit;
+
+import org.apache.commons.codec.binary.Hex;
+
+import org.apache.commons.cli.*;
+
+@Data
+@Slf4j
+public class MeshApp {
+    private static final String APP_NAME = "example_utilities";
+    Reticulum reticulum;
+    //static final String defaultConfigPath = new String(".reticulum");
+    Identity meshIdentity;
+    Transport transport;
+    public Destination baseDestination;
+    private volatile boolean isShuttingDown = false;
+    private final List<RNSPeer> linkedPeers = Collections.synchronizedList(new ArrayList<>());
+    //private final List<Link> incomingLinks = Collections.synchronizedList(new ArrayList<>());
+    //public Link latestClientLink;
+    //public Link meshLink;
+    //public Link clientLink;
+    
+    /************/
+    /** Mesh   **/
+    /************/
+    private void meshSetup(String configPath) {
+
+        try {
+            reticulum = new Reticulum(configPath);
+        } catch (IOException e) {
+            log.error("unable to create Reticulum network", e);
+        }
+
+        // create identity either from file or new (creating new keys)
+        var meshIdentityPath = reticulum.getStoragePath().resolve("identities/"+APP_NAME);
+        if (Files.isReadable(meshIdentityPath)) {
+            meshIdentity = Identity.fromFile(meshIdentityPath);
+            log.info("mesh identity loaded from file {}", meshIdentityPath.toString());
+        } else {
+            meshIdentity = new Identity();
+            log.info("new mesh identity created dynamically.");
+        }
+
+        // We create a destination that clients can connect to. We
+        // want clients to create links to this destination, so we
+        // need to create a "single" destination type.
+        baseDestination = new Destination(
+            meshIdentity,
+            Direction.IN,
+            DestinationType.SINGLE,
+            APP_NAME,
+            "meshexample"
+        );
+        //log.info("destination1 hash: "+destination1.getHexHash());
+   
+        baseDestination.setProofStrategy(ProofStrategy.PROVE_ALL);
+        baseDestination.setAcceptLinkRequests(true);
+
+        // We configure a function that will get called every time
+        // a new client creates a link to this destination.
+        baseDestination.setLinkEstablishedCallback(this::clientConnected);
+        
+        Transport.getInstance().registerAnnounceHandler(new MeshAnnounceHandler());
+        log.info("announceHandlers: {}", Transport.getInstance().getAnnounceHandlers());
+
+        meshLoop(baseDestination);
+    }
+
+    public void meshLoop(Destination destination) {
+        var inData = new String();
+        log.info("***> mesh instance * {} * running, waiting for a connection", destination.getHexHash());
+
+        log.info("Hit enter to manually send an announce (Ctrl-C or 'quit' to quit)");
+
+        Scanner scan = new Scanner( System.in );
+        while (true) {
+            try {
+                inData = scan.nextLine();
+                destination.announce();
+                log.info("Sent announce from {} ({})", destination.getHexHash(), destination.getName());
+                if (inData.equals("quit")) {
+                    shutdown();
+                    scan.close();
+                    break;
+                }
+            } catch (Exception e) {
+                scan.close();
+            }
+        }
+        System.exit(0);
+    }
+
+    public void shutdown() {
+        isShuttingDown = true;
+        log.info("shutting down Reticulum");
+        for (RNSPeer p: linkedPeers) {
+            p.shutdown();
+        }
+        reticulum.exitHandler();
+    }
+
+    public void clientConnected(Link link) {
+        link.setLinkClosedCallback(this::clientDisconnected);
+        //link.setPacketCallback(this::serverPacketReceived);
+        //latestClientLink = link;
+        log.info("***> Client connected");
+    }
+
+    public void clientDisconnected(Link link) {
+        log.info("***> Client disconnected");
+    }
+
+    public void serverPacketReceived(byte[] message, Packet packet) {
+        String text = new String(message, StandardCharsets.UTF_8);
+        log.info("Received data on the link: \"{}\"", text);
+    //    // send reply
+    //    String replyText = "I received \""+text+"\" over the link";
+    //    byte[] replyData = replyText.getBytes(StandardCharsets.UTF_8);
+    //    Packet reply = new Packet(latestClientLink, replyData);
+    //    reply.send();
+    }
+
+    public RNSPeer findPeerByLink(Link link) {
+        List<RNSPeer> lps =  getLinkedPeers();
+        RNSPeer peer = null;
+        for (RNSPeer p : lps) {
+            var pLink = p.getPeerLink();
+            if (Arrays.equals(pLink.getDestination().getHash(),link.getDestination().getHash())) {
+                log.info("found peer matching destinationHash");
+                peer = p;
+                break;
+            }
+        }
+        return peer;
+    }
+
+    /***********************/
+    /** AnnounceHandler   **/
+    /***********************/
+
+    private class MeshAnnounceHandler implements AnnounceHandler {
+        @Override
+        public String getAspectFilter() {
+            return null;
+        }
+
+        @Override
+        public void receivedAnnounce(byte[] destinationHash, Identity announcedIdentity, byte[] appData) {
+            var peerExists = false;
+
+            log.info("Received an announce from {}", Hex.encodeHexString(destinationHash));
+
+            if (nonNull(appData)) {
+                log.debug("The announce contained the following app data: {}", new String(appData, UTF_8));
+            }
+
+            List<RNSPeer> lps =  getLinkedPeers();
+            RNSPeer peer = null;
+            for (RNSPeer p : lps) {
+                // TODO: which parts of the peer need to be checked for equality ???
+                if (Arrays.equals(p.getDestinationHash(), destinationHash)) {
+                    log.info("found peer matching destinationHash");
+                    peer = p;
+                    break;
+                }
+            }
+            if (peer == null) {
+                RNSPeer newPeer = new RNSPeer(destinationHash);
+                //newPeer.init(destinationHash);
+                newPeer.setDestinationIdentity(announcedIdentity);
+                newPeer.setIsInitiator(true);
+                lps.add(newPeer);
+                log.info("added new RNSPeer, destinationHash: {}", Hex.encodeHexString(destinationHash));
+            }
+        }
+
+    }
+
+    /************/
+    /** Peer   **/
+    /************/
+    @Data
+    private static class RNSPeer {
+        byte[] destinationHash;
+        Destination peerDestination;
+        Identity destinationIdentity;
+        Long creationTimestamp;
+        Long lastAccessTimestamp;
+        Boolean isInitiator;
+        Link peerLink;
+
+        public RNSPeer(byte[] dhash) {
+            destinationHash = dhash;
+            destinationIdentity = recall(dhash);
+
+            peerDestination = new Destination(
+                destinationIdentity,
+                Direction.OUT, 
+                DestinationType.SINGLE,
+                APP_NAME,
+                "peerexample"
+            );
+            peerDestination.setProofStrategy(ProofStrategy.PROVE_ALL);
+
+            setCreationTimestamp(System.currentTimeMillis());
+            lastAccessTimestamp = null;
+            isInitiator = true;
+
+            peerLink = new Link(peerDestination);
+
+            this.peerLink.setLinkEstablishedCallback(this::linkEstablished);
+            this.peerLink.setLinkClosedCallback(this::linkClosed);
+            this.peerLink.setPacketCallback(this::linkPacketReceived);
+        }
+
+        public void shutdown() {
+            if (nonNull(peerLink)) {
+                if (peerLink.getStatus() == ACTIVE) {
+                    peerLink.teardown();
+                } else {
+                    peerLink = null;
+                }
+            }
+        }
+
+        public void linkEstablished(Link link) {
+            peerLink = link;
+            peerLink.setLinkClosedCallback(this::linkClosed);
+            log.info("Link {} established with peer: hash - {}, identity: {}", peerLink, destinationHash, destinationIdentity);
+        }
+
+        public void linkClosed(Link link) {
+            if (link.getTeardownReason() == TIMEOUT) {
+                log.info("The link timed out");
+            } else if (link.getTeardownReason() == INITIATOR_CLOSED) {
+                log.info("Link closed callback: The initiator closed the link");
+            } else if (link.getTeardownReason() == DESTINATION_CLOSED) {
+                log.info("Link closed callback: The link was closed by the peer, removing peer");
+            } else {
+                log.info("Link closed callback");
+            }
+        }
+
+        public void linkPacketReceived(byte[] message, Packet packet) {
+            var msgText = new String(message, StandardCharsets.UTF_8);
+            if (msgText.equals("ping")) {
+                log.info("received ping on link");
+            }
+        }
+    }
+
+
+    /*********/
+    /** Main */
+    /*********/
+    public static void main(String[] args) throws IOException {
+        // main to run application directly
+
+        String cmdUsage = new String("run_mesh.sh [-h|-config PATH]");
+
+         String defaultConfigPath = new String(".reticulum");
+        
+        // define options
+        Options options = new Options();
+        Option o_help = Option.builder("h").longOpt("help")
+                            .argName("usage")
+                            .hasArg(false)
+                            .required(false)
+                            .desc("program usage")
+                            .build();
+        options.addOption(o_help);
+        Option o_config = Option.builder("c").longOpt("config")
+                            .argName("dir")
+                            .hasArg(true)
+                            .required(false)
+                            .desc("(optional) path to alternative Reticulum config directory "
+                                  + "(default: .reticulum)").build();
+        options.addOption(o_config);
+        // define parser
+        CommandLine cLine;
+        CommandLineParser parser = new DefaultParser();
+        HelpFormatter formatter = new HelpFormatter();
+        
+        try {
+            cLine = parser.parse(options, args);
+        
+            if (cLine.hasOption("h")) {
+                formatter.printHelp(cmdUsage, options);
+                System.exit(0);
+            }
+
+            var instance = new MeshApp();
+
+            if (cLine.hasOption("config")) {
+                try {
+                    instance.meshSetup(cLine.getOptionValue("c"));
+                } catch (IllegalArgumentException e) {
+                    log.error("Invalid config path. Check your input!");
+                    System.exit(0);
+                }
+            } else {
+                instance.meshSetup(defaultConfigPath);
+            }
+
+        } catch (ParseException e) {
+            e.printStackTrace();
+            System.out.println(e.getMessage());
+            formatter.printHelp(cmdUsage, options);
+            System.exit(0);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
