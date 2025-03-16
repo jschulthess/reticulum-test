b758e02eb5571147128a59863a6d6dd9ad99bf6a
Jurg Schulthess <schulthess@puzzle.ch>
2025-03-16 17:24:49 +0100

    enclose received string in quotes

--- java/example-app/src/main/java/io/reticulum/examples/MeshAppBuffer.java
+++ java/example-app/src/main/java/io/reticulum/examples/MeshAppBuffer.java
@@ -637,7 +637,7 @@ public class MeshAppBuffer {
             var data = this.peerBuffer.read(readyBytes);
             var decodedData = new String(data);
 
-            log.info("Received data ({} bytes) over the buffer: {}", readyBytes, decodedData);
+            log.info("Received data ({} bytes) over the buffer: \"{}\"", readyBytes, decodedData);
 
             // process data. In this example: reply data back to client
             if (nonNull(doReply) & isTrue(doReply) & (isFalse(this.isInitiator))) {
