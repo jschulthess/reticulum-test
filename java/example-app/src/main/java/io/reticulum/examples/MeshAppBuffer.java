dfa16b77417b06d332567247a640f9076e29365c
Jurg Schulthess <schulthess@puzzle.ch>
2025-03-16 17:50:03 +0100

    show data without testing

--- java/example-app/src/main/java/io/reticulum/examples/MeshAppBuffer.java
+++ java/example-app/src/main/java/io/reticulum/examples/MeshAppBuffer.java
@@ -638,10 +638,8 @@ public class MeshAppBuffer {
             var data = this.peerBuffer.read(readyBytes);
             var decodedData = new String(data);
 
-            if (decodedData != "") {
-                log.info("Received data ({} bytes) over the buffer: \"{}\"", readyBytes, decodedData);
-            }
-
+            log.info("Received data ({} bytes) {} over the buffer: \"{}\"", readyBytes, data, decodedData);
+            
             // process data. In this example: reply data back to client
             if (nonNull(doReply) & isTrue(doReply) & (isFalse(this.isInitiator))) {
                 var replyText = "I received ** "+decodedData;
